892072d530e9868b270507fa32bd3cb4bde8a443
Ell534 <elliot94b@gmail.com>
2024-03-06 18:21:53 +0000

    complete ex9

---
+++ ex9/ex9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int i = 25;
+  while (i > -1) {
+    printf("%d\n", i);
+    i--;
+  }
+
+  // need this to add a final newline
+  printf("\n");
+
+  return 0;
+}
